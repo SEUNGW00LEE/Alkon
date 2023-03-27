f1d44603afb342a5fea488db93dfd77f1467bddd
seungwoo <seungwoolee99@gmail.com>
2023-03-27 18:27:01 +0900

    Alkon1773

---
+++ Alkon/Alkon1773.py
@@ -0,0 +1,12 @@
+number, time = map(int, input().split())
+space = [0] * (time+1)
+
+for i in range(number) :
+  num = int(input())
+  if num == 1: #시간초과
+    print(time)
+    quit()
+  for s in range(num, time+1, num):
+    space[s] = 1
+
+print(sum(space))
